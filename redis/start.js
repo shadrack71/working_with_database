032002dd55ee736e54d6d8078b10f746b5a58318
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-02-27 23:49:36 +0300

    feat: add Redis client setup and basic operations in start.js

---
+++ redis/start.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+    username: 'default',
+    password: 'amZ1aexhad61DWAa1ZWvqXct6j7bya2s',
+    socket: {
+        host: 'redis-18091.c91.us-east-1-3.ec2.cloud.redislabs.com',
+        port: 18091
+    }
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
+await client.set('foo', 'bar');
+const result = await client.get('foo');
+console.log(result)  // >>> bar
+
