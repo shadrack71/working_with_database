91c6c88a4bddde9ebb9ec36ff46a97c486fd4105
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-02-28 04:24:58 +0300

    feat: add book creation logic and display books in the main page

--- redis/src/app/actions/create.js
+++ redis/src/app/actions/create.js
@@ -8,6 +8,16 @@ export async function createBook(formData) {
 
   const id = Math.floor(Math.random()*100000)
 
+  const unique = await client.zAdd('storybooks',{
+    value:title,
+    score:id
+  },{NX:true})
+  if(!unique){
+
+    return { error:'The book already existed' }
+
+  }
+
   await client.hSet(`storybooks:${id}`,{
     title,
     rating,
--- redis/src/app/page.jsx
+++ redis/src/app/page.jsx
@@ -1,12 +1,22 @@
-'use client'
+import client from "@/app/lib/db"
+import Link from 'next/link'
 
-const Link = require ( 'next/link')
 
 const getBooks = async () => {
+
+  const result = await client.zRangeWithScores('storybooks',0,-1)
+  const books = await Promise.all(result.map(async(b)=>{
+    return await client.hGetAll(`storybooks:${b.score}`)
+  }))
+
+  return books
+
   
 }
 
 export default async function Home() {
+
+  const books = await getBooks()
 
   return (
     <main>
@@ -16,6 +26,16 @@ export default async function Home() {
       </nav>
       
       <p>List of books here.</p>
+      {
+          books.map((book, index) => (
+          <div key={`${book.title}-${index}`} className="card">
+          <h2>{book.title}</h2>
+          <p>By {book.author}</p>
+          <p>{book.blurb}</p>
+          <p>Rating: {book.rating}</p>
+        </div>
+      ))
+      }
     </main>
   )
 }
